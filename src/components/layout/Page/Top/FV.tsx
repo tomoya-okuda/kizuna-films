20d7171ab1a5d6e4458a3e24f919e5b72a330b57
tomoya-okuda <tomoya.okuda@necmos.jp>
2023-07-12 17:28:59 +0900

    FVにヒーロー画像を追加

--- src/components/layout/Page/Top/FV.tsx
+++ src/components/layout/Page/Top/FV.tsx
@@ -1,14 +1,26 @@
 import CountDate from '@/components/ui/Others/CountDate';
 import CountTime from '@/components/ui/Others/CountTime';
+import Image from 'next/image';
 
 type Props = {};
 
 function FV({}: Props) {
   return (
-    <div className="border-b-black100 py-6 px-3">
-      <div className="flex justify-between">
+    <div className="relative border-b-black100 py-6 px-3 min-h-[80vh] w-full">
+      {/* 日付と時間 */}
+      <div className="flex justify-between mb-2">
         <CountDate />
         <CountTime />
+      </div>
+      {/* ヒーローセクション */}
+      <div className="relative w-full min-h-[468px]">
+        <Image
+          src="/images/hero.avif"
+          alt="thumbnail"
+          loading="eager"
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
