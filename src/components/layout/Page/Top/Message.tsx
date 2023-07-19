9a5215ca4ce71eec4d11d9395ebbfeb7197721dd
tomoya-okuda <tomoya.okuda@necmos.jp>
2023-07-19 23:24:50 +0900

    Message CSS修正

--- src/components/layout/Page/Top/Message.tsx
+++ src/components/layout/Page/Top/Message.tsx
@@ -2,7 +2,7 @@ type Props = {};
 
 function Message({}: Props) {
   return (
-    <div className="border-b-black100 flex flex-col gap-9 py-[64px] px-[40px]">
+    <div className="border-b-black100 flex flex-col gap-9 py-[80px] px-[40px]">
       <h2>
         一生の思い出となる瞬間と、<br></br> 2人のキズナを映像化する。
       </h2>
@@ -10,7 +10,9 @@ function Message({}: Props) {
         特別な日、一生に一度の結婚式を、
         美しいオープニングムービーに刻み込む。それが私たちのサービスの目的です。
       </p>
-      <p>提供する映像は、ただの記録ではありません。</p>
+      <p>
+        提供する映像は、<br></br>ただの記録ではありません。
+      </p>
       <p>2人の感情、絆、時間の価値を反映し、物語を紡ぐ記念品となります。</p>
     </div>
   );
